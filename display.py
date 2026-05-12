a26f655e38c1f040396c5d8936760345cae07af9
Pawan-Koder <1213056@genstudents.org>
2026-05-12 13:39:02 +0100

    Neater version of project, partial sprint 4

--- display.py
+++ display.py
@@ -4,3 +4,10 @@ def display_header(section_name):
     print(f"{'CAFE PYTHONS':^60}")
     print(f"{section_name:^60}")
     print("=" * 60)
+
+# DISPLAY_PRODUCT_NAVIGATION_MENU TO BE MOVED HERE
+
+# CREATE FUNCTION FOR DISPLAY_ORDER_MENU
+
+# CREATE FUNCTION FOR DISPLAY_COURIER_MENU
+
